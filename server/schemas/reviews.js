ea5d588cccc009c99cb5088e4d017a47f7966651
Vivian Zhang <vvynz12@gmail.com>
2022-11-24 18:25:04 -0500

    Create & setup a reviews schema and import it in the schema.js file

---
+++ server/schemas/reviews.js
@@ -0,0 +1,27 @@
+export default {
+  name: "reviews",
+  title: "Reviews",
+  type: "document",
+  fields: [
+    {
+      name: "name",
+      title: "Name",
+      type: "string"
+    },
+    {
+      name: "company",
+      title: "Company",
+      type: "string"
+    },
+    {
+      name: "articleLink",
+      title: "Article Link",
+      type: "string"
+    },
+    {
+      name: "review",
+      title: "Review",
+      type: "string"
+    },
+  ]
+}
