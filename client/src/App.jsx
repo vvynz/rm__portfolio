f4ad7bef44573eb2c80965e02892e60309fde57a
Vivian Zhang <vvynz12@gmail.com>
2022-11-24 19:20:49 -0500

    Create & setup the Header and Footer components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,19 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
-import About from './container/About/About'
+import About from "./container/About/About";
+import Header from "./container/Header/Header";
+import Footer from "./container/Footer/Footer";
 
 function App() {
-
   return (
     <div className="App">
       <h1>HELLO</h1>
+      <About />
+      <Header />
+      <Footer />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ client/src/container/Header/Header.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import "./Header.scss";
+
+export default function Header() {
+  return(
+    <h1>Header</h1>
+  )
+}
