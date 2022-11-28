bbaf9851ec3ab332fe9cb841e91efa5f8fa547a5
Vivian Zhang <vvynz12@gmail.com>
2022-11-28 18:01:33 -0500

    Style Footer

--- client/src/container/Footer/Footer.jsx
+++ client/src/container/Footer/Footer.jsx
@@ -3,7 +3,24 @@ import React from "react";
 import "./Footer.scss";
 
 export default function Footer() {
-  return(
-    <h1>Footer</h1>
-  )
+  return (
+    <>
+      <div className="app__footer">
+        <div>
+          <ul className="app__footer-social-links">
+            {["Instagram", "Twitter", "Spotify", "Youtube"].map((item) => (
+              <li key={item}>
+                <a href="/" target="_blank">
+                  <img src="/" alt={item} />
+                </a>
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div>
+          
+        </div>
+      </div>
+    </>
+  );
 }
