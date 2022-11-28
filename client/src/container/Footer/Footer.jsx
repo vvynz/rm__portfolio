13d6df3fd17c7daf2b1d75551efd4b84f5d892d4
Vivian Zhang <vvynz12@gmail.com>
2022-11-28 18:53:14 -0500

    Style Footer

--- client/src/container/Footer/Footer.jsx
+++ client/src/container/Footer/Footer.jsx
@@ -6,7 +6,7 @@ export default function Footer() {
   return (
     <>
       <div className="app__footer">
-        <div>
+        <div className="app__footer-socials">
           <ul className="app__footer-social-links">
             {["Instagram", "Twitter", "Spotify", "Youtube"].map((item) => (
               <li key={item}>
@@ -17,8 +17,8 @@ export default function Footer() {
             ))}
           </ul>
         </div>
-        <div>
-          
+        <div className="app__footer-credits">
+          <p>© 2022 Big Hit Music / Hybe - Website Design by V.Z.</p>
         </div>
       </div>
     </>
