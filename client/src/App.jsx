58b0486a033edeee42c337edb08fc7c318f73a9b
Vivian Zhang <vvynz12@gmail.com>
2022-11-28 15:44:26 -0500

    Create & setup Navbar component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import "./App.css";
 
+import Navbar from "./components/Navbar/Navbar";
 import About from "./container/About/About";
 import Header from "./container/Header/Header";
 import Footer from "./container/Footer/Footer";
@@ -8,6 +9,7 @@ import Footer from "./container/Footer/Footer";
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <h1>HELLO</h1>
       <About />
       <Header />
---
+++ client/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+import "./Navbar.scss";
+
+export default function Navbar() {
+  return <h3>Nav</h3>;
+}
