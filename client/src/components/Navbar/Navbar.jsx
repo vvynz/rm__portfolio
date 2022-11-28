39c1b728b7b3b82d74e802a4147e664baa339b9c
Vivian Zhang <vvynz12@gmail.com>
2022-11-28 17:28:41 -0500

    Minor styling update to Navbar

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -5,6 +5,10 @@ import "./Navbar.scss";
 export default function Navbar() {
   return (
     <nav className="app__navbar">
+      <div className="app__logo">
+        <p>rm___</p>
+        <small>just an archive</small>
+      </div>
       <ul className="app__navbar-links">
         {["home", "about", "work"].map((item) => (
           <li key={`link-${item}`}>
