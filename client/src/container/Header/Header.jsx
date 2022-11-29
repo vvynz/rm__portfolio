816b46ded2e90ff10d444f4323bf4b74ae8a2a5a
Vivian Zhang <vvynz12@gmail.com>
2022-11-29 18:23:13 -0500

    Add header image to Header component & add minor styling

--- client/src/container/Header/Header.jsx
+++ client/src/container/Header/Header.jsx
@@ -7,7 +7,8 @@ import "./Header.scss";
 export default function Header() {
   return(
     <section className="app__header">
-      <div>
+      <div className="app__header-img" />
+      <div className="app_header-news">
         <p>INDIGO OUT DEC 2</p>
       </div>
     </section>
