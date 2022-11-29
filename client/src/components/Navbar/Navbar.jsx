ee8b39cf7af66d6aceead21b70ae854dbcce424c
Vivian Zhang <vvynz12@gmail.com>
2022-11-28 19:01:17 -0500

    Create an index.js in constants folder so we can export the images. Update the logo in the Navbar

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 
+import { images } from "../../constants";
 import "./Navbar.scss";
 
 export default function Navbar() {
   return (
     <nav className="app__navbar">
       <div className="app__logo">
-        <p>rm___</p>
-        <small>just an archive</small>
+        <img src={images.logo} alt="logo" />
+        {/* <p>rm___</p> */}
+        {/* <small>just an archive</small> */}
       </div>
       <ul className="app__navbar-links">
         {["home", "about", "work"].map((item) => (
