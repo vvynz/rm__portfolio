51286ff94f403a6ff8fec0a2a94c89ff7f96f8b7
Vivian Zhang <vvynz12@gmail.com>
2022-12-01 16:51:35 -0500

    Create & import an EmbededVideo component

---
+++ client/src/components/EmbededVideo.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function EmbededVideo() {
+  return <p>Embeded Video</p>;
+}
--- client/src/container/Release/Release.jsx
+++ client/src/container/Release/Release.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 
+import { EmbededVideo } from "../../components";
+
 import "./Release.scss";
 
 export default function Release() {
-  return <h3>Release section</h3>;
+  return (
+    <>
+      <h3>Release section</h3>
+      <EmbededVideo />
+    </>
+  );
 }
