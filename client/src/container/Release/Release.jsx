777009a2d9063c20247c728f7a3d37a223907635
Vivian Zhang <vvynz12@gmail.com>
2022-12-01 19:28:50 -0500

    set up link & EmbededVideo for new release

--- client/src/container/Release/Release.jsx
+++ client/src/container/Release/Release.jsx
@@ -10,6 +10,13 @@ export default function Release() {
       <h1 className="app__release-title">Latest Release</h1>
       <p>"Wild Flower" Official Teaser - Out Now</p>
       <EmbededVideo videoID="fYiDCh-GYoE" />
+
+      <p>INDIGO ALBUM OUT NOW</p>
+      <a href="https://ingrv.es/Indigo" target="_blank">
+        BUY HERE
+      </a>
+      <EmbededVideo />
+      {/* Update with mv link when it is released */}
     </section>
   );
 }
