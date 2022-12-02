76b9ced047a4a1ba518b4dc2fe7c41e55cf35023
Vivian Zhang <vvynz12@gmail.com>
2022-12-02 17:38:19 -0500

    Add new image file

--- client/src/constants/images.js
+++ client/src/constants/images.js
@@ -3,5 +3,6 @@ import instagram from "../assets/instagram.svg";
 import spotify from "../assets/spotify.svg";
 import youtube from "../assets/youtube.svg";
 import indigoHeader from "../assets/indigo_header.jpeg";
+import wildflower from "../assets/wildflower.gif";
 
-export default { indigoHeader, logo, instagram, spotify, youtube };
+export default { indigoHeader, logo, instagram, spotify, youtube, wildflower };
