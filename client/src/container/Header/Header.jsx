2cb83440da079f81d1d382dbe6b682d41621b043
Vivian Zhang <vvynz12@gmail.com>
2022-12-02 18:01:21 -0500

    Update header image & styling

--- client/src/container/Header/Header.jsx
+++ client/src/container/Header/Header.jsx
@@ -6,8 +6,11 @@ import "./Header.scss";
 
 export default function Header() {
   return (
-    <section className="app__header">
-      <div className="app__header-img" />
+    <section className="app__header-container">
+      {/* <div className="app__header-img" /> */}
+      <div className="app__header-img">
+      <img src={images.wildflower} alt="Wild Flower music video" />
+      </div>
       <div className="app_header-news">
         <p>INDIGO OUT DEC 2</p>
         <a className="app__btn" href="https://ingrv.es/Indigo" target="_blank">
