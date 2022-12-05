773be12474cfc1b02e85ab66f5ac0b249455279e
Vivian Zhang <vvynz12@gmail.com>
2022-12-05 17:17:10 -0500

    Styling update

--- client/src/components/Article/Article.jsx
+++ client/src/components/Article/Article.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 
+import "./Article.scss";
+
 export default function Article({ title, link, excerpt }) {
   return (
-    <div>
+    <div className="app__article">
       <h1>{title}</h1>
       <a href={link} target="_blank">
-        {" "}
         Read Here
       </a>
       <p>{excerpt}</p>
