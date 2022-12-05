f318a8793f734b3354fb4413716721333e90d692
Vivian Zhang <vvynz12@gmail.com>
2022-12-05 16:52:26 -0500

    Create an array of articles to loop over & render with the Article component

--- client/src/components/Article/Article.jsx
+++ client/src/components/Article/Article.jsx
@@ -1,5 +1,10 @@
 import React from "react";
 
-export default function Article() {
-  return "ARTICLE ";
+export default function Article({title, link}) {
+  return(
+    <div>
+      <h1>{title}</h1>
+        <a href={link} target="_blank"> Read Here</a>
+    </div>
+  );
 }
--- client/src/container/News/News.jsx
+++ client/src/container/News/News.jsx
@@ -1,18 +1,22 @@
 import React from "react";
 
-import "./News.scss"
+import "./News.scss";
 
 import { Article } from "../../components";
 
+const articles = [
+  {
+    title: "BTS’ RM on His ‘Indigo’ Album, Duets With Erykah Badu and Anderson .Paak, Military Service and Why Solo Careers Will Bolster the Band",
+    link: "https://variety.com/2022/music/news/bts-rm-indigo-solo-album-erykah-badu-anderson-paak-military-wildflower-band-1235447669/"
+  },
+]
 export default function News() {
   return (
     <section className="app__news-container">
       <h1>The Latest</h1>
-      <div>
-        "RS - Indigo is an adventurous sonic portrait of BTS' RM's inner world"
-        <a href="https://www.rollingstone.com/music/music-album-reviews/review-rm-indigo-1234638839/" target="_blank"> Read Here</a>
-        <Article />
-      </div>
+      {articles.map((article) => (
+        <Article title={article.title} link={article.link} />
+      ))}
     </section>
   );
 }
