d232d05898daa74613df496091a2570d9ebf046d
Vivian Zhang <vvynz12@gmail.com>
2022-12-05 16:34:56 -0500

    Create & setup an Article component

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,2 +1,3 @@
 export { default as Navbar } from "./Navbar/Navbar";
 export { default as EmbededVideo } from "./EmbededVideo";
+export { default as Article } from "./Article/Article";
--- client/src/container/News/News.jsx
+++ client/src/container/News/News.jsx
@@ -1,5 +1,18 @@
 import React from "react";
 
+import "./News.scss"
+
+import { Article } from "../../components";
+
 export default function News() {
-  return "THE LATEST";
+  return (
+    <section className="app__news-container">
+      <h1>The Latest</h1>
+      <div>
+        "RS - Indigo is an adventurous sonic portrait of BTS' RM's inner world"
+        <a href="https://www.rollingstone.com/music/music-album-reviews/review-rm-indigo-1234638839/" target="_blank"> Read Here</a>
+        <Article />
+      </div>
+    </section>
+  );
 }
