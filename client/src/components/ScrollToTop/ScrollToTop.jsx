953b6a944f3b434b6f6f9634bae469dfcc92ae81
Vivian Zhang <vvynz12@gmail.com>
2022-12-06 18:35:35 -0500

    Create a state isVisible and a ScrollToTop function

--- client/src/components/ScrollToTop/ScrollToTop.jsx
+++ client/src/components/ScrollToTop/ScrollToTop.jsx
@@ -1,13 +1,22 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
 
 import "./ScrollToTop.scss";
 
-import { images } from '../../constants';
+import { images } from "../../constants";
 
 export default function ScrollToTop() {
-  return(
+  const [isVisible, setIsVisible] = useState(false);
+
+  const scrollTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
+
+  return (
     <>
-    <img className='up-icon' src={images.up} />
+      <img className="up-icon" src={images.up} />
     </>
   );
 }
