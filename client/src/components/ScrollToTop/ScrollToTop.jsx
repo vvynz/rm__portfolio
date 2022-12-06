2b2667a0e87302e4831f1c18872b023362ad8fb7
Vivian Zhang <vvynz12@gmail.com>
2022-12-06 18:58:28 -0500

    Implement a toggleVisibility function wrapped in the useEffect hook

--- client/src/components/ScrollToTop/ScrollToTop.jsx
+++ client/src/components/ScrollToTop/ScrollToTop.jsx
@@ -7,16 +7,36 @@ import { images } from "../../constants";
 export default function ScrollToTop() {
   const [isVisible, setIsVisible] = useState(false);
 
-  const scrollTop = () => {
+  const scrollToTop = () => {
     window.scrollTo({
       top: 0,
       behavior: "smooth",
     });
   };
 
+  useEffect(() => {
+    // button will be displayed after scrolling 500px
+    const toggleVisibility = () => {
+      if (window.pageYOffset > 500) {
+        setIsVisible(true);
+      } else {
+        setIsVisible(false);
+      }
+    };
+
+    window.addEventListener("scroll", toggleVisibility);
+
+    // will stop listening when component is unmounted
+    return () => window.removeEventListener("scroll", toggleVisibility);
+  }, []);
+
   return (
     <>
-      <img className="up-icon" src={images.up} />
+      {isVisible && (
+        <button onClick={scrollToTop}>
+          <img className="up-icon" src={images.up} />
+        </button>
+      )}
     </>
   );
 }
