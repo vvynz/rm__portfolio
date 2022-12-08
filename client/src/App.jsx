40f2628d64ea86748a21c0ac7093f023dc6d1853
Vivian Zhang <vvynz12@gmail.com>
2022-12-08 18:02:29 -0500

    Add album details & style the About component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,8 +9,8 @@ function App() {
     <div className="App">
       <Navbar />
       <Header />
+      <About />
       <Release />
-      {/* <About /> */}
       <News />
       <Footer />
       <ScrollToTop />
--- client/src/container/About/About.jsx
+++ client/src/container/About/About.jsx
@@ -3,7 +3,25 @@ import React from "react";
 import "./About.scss";
 
 export default function About() {
-  return(
-    <h1>About Sec</h1>
-  )
+  return (
+    <section className="app__about">
+      <p>
+        RM of 21st century pop icons BTS, released a new solo album, Indigo on
+        Dec 2.
+      </p>
+      <p>
+        Like a diary, Indigo recounts the storiees and experience RM has gone
+        through. It is a record of his 20s and a candid introduction of who RM
+        truly is.
+      </p>
+      <p>
+        The album comprises of 10 tracks in various genres and with several
+        featured artists, including hte lead single "Wild Flower (with
+        youjeen)." 
+        RM led the overall album making from writing the songs and
+        lyrics, developing the concept of the album, designing album art to
+        develop the music video.
+      </p>
+    </section>
+  );
 }
