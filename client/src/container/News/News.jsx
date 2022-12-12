f8e8637997b2aa93d8b1411e1d35f87c661026cd
Vivian Zhang <vvynz12@gmail.com>
2022-12-12 17:30:44 -0500

    Import necessary Swiper react components & modules

--- client/src/container/News/News.jsx
+++ client/src/container/News/News.jsx
@@ -1,4 +1,12 @@
 import React from "react";
+// import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+// Import required Swiper modules
+import { Autoplay, Pagination, Navigation } from "swiper";
+
+import "swiper/scss";
+import "swiper/scss/pagination";
+import "swiper/scss/navigation";
 
 import "./News.scss";
 
@@ -7,43 +15,54 @@ import { Article } from "../../components";
 const articles = [
   {
     id: 1,
-    title: "BTS Leader RM Pushes Against the Weight of Fame, Looks Towards a Bright Future on 'Indigo'",
-    link: "https://www.rollingstone.com/music/music-album-reviews/review-rm-indigo-1234638839/"
+    title:
+      "BTS Leader RM Pushes Against the Weight of Fame, Looks Towards a Bright Future on 'Indigo'",
+    link: "https://www.rollingstone.com/music/music-album-reviews/review-rm-indigo-1234638839/",
   },
   {
     id: 2,
-    title: "BTS’ RM on His ‘Indigo’ Album, Duets With Erykah Badu and Anderson .Paak, Military Service and Why Solo Careers Will Bolster the Band",
-    link: "https://variety.com/2022/music/news/bts-rm-indigo-solo-album-erykah-badu-anderson-paak-military-wildflower-band-1235447669/"
+    title:
+      "BTS’ RM on His ‘Indigo’ Album, Duets With Erykah Badu and Anderson .Paak, Military Service and Why Solo Careers Will Bolster the Band",
+    link: "https://variety.com/2022/music/news/bts-rm-indigo-solo-album-erykah-badu-anderson-paak-military-wildflower-band-1235447669/",
   },
   {
     id: 3,
-    title: "RM of BTS: “My whole twenties were an exhibition… but I chose to be a star”",
-    link: "https://www.nme.com/features/music-features/bts-rm-interview-indigo-3356430"
+    title:
+      "RM of BTS: “My whole twenties were an exhibition… but I chose to be a star”",
+    link: "https://www.nme.com/features/music-features/bts-rm-interview-indigo-3356430",
   },
   {
     id: 4,
     title: "With Indigo, RM Makes Fleeting Moments Feel Permanent",
     link: "https://consequence.net/2022/12/rm-indigo-review-bts-album-analysis/",
-    excerpt: "Indigo contains many things — an artist expressing the frustrations of a uniquely isolating chapter of life, creative catharsis, room for experimentation, poignant unraveling of heartbreak and hope. Most of all, though, it’s a record from a writer at the very top of his game who has proven that he still has so far to go and so much to share. RM paints with brushstrokes of his own that are never broad, and always intentional"
+    excerpt:
+      "Indigo contains many things — an artist expressing the frustrations of a uniquely isolating chapter of life, creative catharsis, room for experimentation, poignant unraveling of heartbreak and hope. Most of all, though, it’s a record from a writer at the very top of his game who has proven that he still has so far to go and so much to share. RM paints with brushstrokes of his own that are never broad, and always intentional",
   },
   {
     id: 5,
     title: "RM OF BTS IS EMBRACING THE SILENCE",
-    link: "https://www.theatlantic.com/culture/archive/2022/12/rm-bts-indigo-album/672307/"
+    link: "https://www.theatlantic.com/culture/archive/2022/12/rm-bts-indigo-album/672307/",
   },
   {
     id: 6,
-    title: "'It Documents My Growth as an Artist and a Person': RM on Creativity, Collaboration, and His First Solo Album",
+    title:
+      "'It Documents My Growth as an Artist and a Person': RM on Creativity, Collaboration, and His First Solo Album",
     link: "https://www.vogue.com/article/rm-bts-indigo-interview",
-    excerpt: "“Indigo still explores my emotions and learnings through my experiences.” The shift toward a more intimate kind of storytelling is something he hopes will resonate with listeners. “It’s more open in a way people can easily relate to”"
-  }
-]
+    excerpt:
+      "“Indigo still explores my emotions and learnings through my experiences.” The shift toward a more intimate kind of storytelling is something he hopes will resonate with listeners. “It’s more open in a way people can easily relate to”",
+  },
+];
 export default function News() {
   return (
     <section id="news" className="app__news-container">
       <h1 className="app__news-container-title">News</h1>
       {articles.map((article) => (
-        <Article key={article.id} title={article.title} link={article.link} excerpt={article.excerpt} />
+        <Article
+          key={article.id}
+          title={article.title}
+          link={article.link}
+          excerpt={article.excerpt}
+        />
       ))}
     </section>
   );
