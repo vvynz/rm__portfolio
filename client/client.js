b80bb2c671253fe993679881fa0b655e86d04f20
Vivian Zhang <vvynz12@gmail.com>
2022-12-13 17:44:05 -0500

    Update client so that it can access the Sanity client for images

--- client/client.js
+++ client/client.js
@@ -1,4 +1,5 @@
 import sanityClient from "@sanity/client";
+import imageUrlBuilder from "@sanity/image-url";
 
 export const client = sanityClient({
   projectId: process.env.REACT_APP_SANITY_PROJECT_ID,
@@ -7,3 +8,7 @@ export const client = sanityClient({
   useCdn: true,
   token: process.env.REACT_APP_SANITY_TOKEN,
 });
+
+const builder = imageUrlBuilder(client);
+
+export const urlFor = (source) => builder.image(source);
