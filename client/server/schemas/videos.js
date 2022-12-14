11fc277988619b87744f24f009b2a9ac1a1e4461
Vivian Zhang <vvynz12@gmail.com>
2022-12-13 20:05:02 -0500

    Create a schema for videos

---
+++ client/server/schemas/videos.js
@@ -0,0 +1,17 @@
+export default {
+  name: "videos",
+  title: "Videos",
+  type: "document",
+  fields: [
+    {
+      name: "title",
+      title: "Title",
+      type: "string",
+    },
+    {
+      name: "videoID",
+      title: "videoID",
+      type: "string",
+    },
+  ],
+};
