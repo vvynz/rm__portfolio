35f8019f0e9a8e51021fbd282920a628779ec01b
Vivian Zhang <vvynz12@gmail.com>
2022-12-15 18:22:04 -0500

    Fetch the discography data & save to state using the useEffect hook

--- client/src/container/Discography/Discography.jsx
+++ client/src/container/Discography/Discography.jsx
@@ -7,6 +7,13 @@ import { client } from "../../../client";
 export default function Discography() {
   const [discography, setDiscography] = useState([]);
 
+  useEffect(() => {
+    const query = "*[_type == 'discography']";
+
+    client.fetch(query).then((data) => setDiscography(data));
+  }, []);
+  console.log(discography);
+
   return (
     <section className="app__discography">
       <h1>DISCOGRAPHY</h1>
