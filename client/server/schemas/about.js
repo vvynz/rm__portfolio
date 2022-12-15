81474c5ef0c23037318d318fa593e78d2b71ccb0
Vivian Zhang <vvynz12@gmail.com>
2022-12-15 16:23:11 -0500

    Update the about schema fields

--- client/server/schemas/about.js
+++ client/server/schemas/about.js
@@ -1,6 +1,13 @@
 export default {
   title: "About",
   name: "about",
-  type: "text",
-  description: "Enter a short description about yourself or your latest work"
+  type: "document",
+  fields: [
+    {
+      name: "about",
+      title: "About",
+      type: "text",
+      description: "Enter a short description about yourself or your latest work"
+    }
+  ]
 };
