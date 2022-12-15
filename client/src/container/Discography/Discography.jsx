1c4f58d9b6822da63332057346cfc6bc10c4bbb4
Vivian Zhang <vvynz12@gmail.com>
2022-12-15 18:35:08 -0500

    Render the dynamic data from state

--- client/src/container/Discography/Discography.jsx
+++ client/src/container/Discography/Discography.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 
 import "./Discography.scss";
 
-import { client } from "../../../client";
+import { client, urlFor } from "../../../client";
 
 export default function Discography() {
   const [discography, setDiscography] = useState([]);
@@ -17,6 +17,17 @@ export default function Discography() {
   return (
     <section className="app__discography">
       <h1>DISCOGRAPHY</h1>
+      {discography.map((item, index) => (
+        <div key={index}>
+          <h2>{item.title}</h2>
+          <img src={urlFor(item.albumImg)} alt={item.albumImg.caption} />
+          <ul>
+            {item.details.split(",").map((track) => 
+            <li>{track}</li>
+            )}
+          </ul>
+        </div>
+      ))}
     </section>
   );
 }
