8703cf0741ecd93c27e9bd45f46827ef96fdc951
Vivian Zhang <vvynz12@gmail.com>
2022-12-15 14:12:43 -0500

    Create & set up a new schema for discography

---
+++ client/server/schemas/discography.js
@@ -0,0 +1,22 @@
+export default discography = {
+  name: discography,
+  title: Discography,
+  type: document,
+  fields: [
+    {
+      name: "title",
+      title: "",
+      type: "string"
+    },
+    {
+      name: "albumImg",
+      title: "Album image",
+      type: "string"
+    },
+    {
+      name: "details",
+      title: "Details",
+      type: "string"
+    },
+  ]
+}
--- client/server/schemas/schema.js
+++ client/server/schemas/schema.js
@@ -5,6 +5,7 @@ import createSchema from 'part:@sanity/base/schema-creator'
 import schemaTypes from 'all:part:@sanity/base/schema-type'
 import articles from './articles'
 import videos from './videos'
+import discography from './discography';
 
 // Then we give our schema to the builder and provide the result to Sanity
 export default createSchema({
@@ -14,6 +15,7 @@ export default createSchema({
   // to the ones provided by any plugins that are installed
   types: schemaTypes.concat([
     articles,
+    discography,
     videos
   ]),
 })
