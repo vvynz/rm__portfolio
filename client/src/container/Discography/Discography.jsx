2887f6b98c9b75136e705bd360478d9a7dbbe8e6
Vivian Zhang <vvynz12@gmail.com>
2022-12-15 18:19:37 -0500

    Create a state to save the discography data that will be fetched from the sanity client

--- client/src/container/Discography/Discography.jsx
+++ client/src/container/Discography/Discography.jsx
@@ -1,10 +1,14 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 import "./Discography.scss";
 
+import { client } from "../../../client";
+
 export default function Discography() {
+  const [discography, setDiscography] = useState([]);
+
   return (
-    <section>
+    <section className="app__discography">
       <h1>DISCOGRAPHY</h1>
     </section>
   );
