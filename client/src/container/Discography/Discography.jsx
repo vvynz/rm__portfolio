a1b6fec5a6a553db22d8fc4a24d25bfea5845d9e
Vivian Zhang <vvynz12@gmail.com>
2022-12-16 18:00:56 -0500

    Update discography component styling & add xs screen breakpoint styling

--- client/src/container/Discography/Discography.jsx
+++ client/src/container/Discography/Discography.jsx
@@ -12,23 +12,29 @@ export default function Discography() {
 
     client.fetch(query).then((data) => setDiscography(data));
   }, []);
-  console.log(discography);
+  // console.log(discography);
 
   return (
     <section className="app__discography">
       <h1>DISCOGRAPHY</h1>
-      {discography.map((item, index) => (
-        <div key={index} className="app__discography-album">
-          <h2>{item.title}</h2>
-          <img className="discography__album-img" src={urlFor(item.albumImg)} alt={item.albumImg.caption} />
-          <p>Tracklist:</p>
-          <ul>
-            {item.tracklist.split(",").map((track, index) => (
-              <li key={index}>{track}</li>
-            ))}
-          </ul>
-        </div>
-      ))}
+      <div className="app__discography-wrapper">
+        {discography.map((item, index) => (
+          <div key={index} className="app__discography-album">
+            <h2>{item.title}</h2>
+            <img
+              className="discography__album-img"
+              src={urlFor(item.albumImg)}
+              alt={item.albumImg.caption}
+            />
+            <p>Tracklist:</p>
+            <ul>
+              {item.tracklist.split(",").map((track, index) => (
+                <li key={index}>{track}</li>
+              ))}
+            </ul>
+          </div>
+        ))}
+      </div>
     </section>
   );
 }
