624efb48df7b8cc899e8eddda2750d9df7bedb35
Vivian Zhang <vvynz12@gmail.com>
2023-01-12 16:55:00 -0500

    Set up Swiper & Swiper Slide

--- client/src/container/Header/Header.jsx
+++ client/src/container/Header/Header.jsx
@@ -8,7 +8,9 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import { Autoplay, Pagination, Navigation } from "swiper";
 // Import required Swiper modules
 
-
+import "swiper/scss";
+import "swiper/scss/pagination";
+import "swiper/scss/navigation";
 
 import "./Header.scss";
 
@@ -16,33 +18,58 @@ export default function Header() {
   return (
     <section className="app__header-container">
       {/* <div className="app__header-img" /> */}
-      <div className="app__header-img">
-        <img
-          className="header__img"
-          src={images.wildflower}
-          alt="Wild Flower music video"
-        />
-        <a href="https://www.youtube.com/watch?v=u18be_kRmC0" target="_blank">
-          <img
-            className="app__icon"
-            src={images.play}
-            alt="play icon"
-            onLoad={(e) => SVGInject(e.target)}
-          />
-        </a>
-        <div className="header-caption">
-          <p>wildflower</p>
-        </div>
-      </div>
-      {/* <div className="app__header-img">
+      <Swiper
+        modules={[Autoplay, Pagination, Navigation]}
+        centeredSlides={true}
+        autoplay={{
+          delay: 5000,
+          disableOnInteraction: false,
+        }}
+        pagination={{
+          clickable: true,
+        }}
+        navigation
+        scrollbar={{
+          draggable: true,
+        }}
+      >
+        <SwiperSlide>
+          <div className="app__header-img">
+            <img
+              className="header__img"
+              src={images.wildflower}
+              alt="Wild Flower music video"
+            />
+            <a
+              href="https://www.youtube.com/watch?v=u18be_kRmC0"
+              target="_blank"
+            >
+              <img
+                className="app__icon"
+                src={images.play}
+                alt="play icon"
+                onLoad={(e) => SVGInject(e.target)}
+              />
+            </a>
+            <div className="header-caption">
+              <p>wildflower</p>
+            </div>
+          </div>
+          {/* <div className="app__header-img">
         <img className="header" src={images.indigoBanner} alt="Indigo banner" />
       </div> */}
-      <div className="app_header-news">
-        <p>Indigo out now</p>
-        <a className="app__btn" href="https://ingrv.es/Indigo" target="_blank">
-          order now
-        </a>
-      </div>
+          <div className="app_header-news">
+            <p>Indigo out now</p>
+            <a
+              className="app__btn"
+              href="https://ingrv.es/Indigo"
+              target="_blank"
+            >
+              order now
+            </a>
+          </div>
+        </SwiperSlide>
+      </Swiper>
     </section>
   );
 }
