ed5fa3bff4d6c00c5e79da4cb915f25c7ec55e61
Vivian Zhang <vvynz12@gmail.com>
2023-02-13 17:56:10 -0500

    implement the Section function

--- client/src/container/MediaReleases/MediaReleases.jsx
+++ client/src/container/MediaReleases/MediaReleases.jsx
@@ -7,7 +7,22 @@ import { client } from "../../../client";
 import "./MediaReleases.scss";
 
 function Section({ children }) {
-  
+  const ref = useRef(null);
+  const isInView = useInView(ref, { once: true });
+
+  return (
+    <section>
+      <span
+        style={{
+          transform: isInView ? "none" : "translateX(-200px)",
+          opacity: isInView ? 1 : 0,
+          transition: "all 0.9s cubic-bezier(0.17, 0.55, 0.55, 1) 0.5s",
+        }}
+      >
+        {children}
+      </span>
+    </section>
+  );
 }
 
 export default function Release() {
