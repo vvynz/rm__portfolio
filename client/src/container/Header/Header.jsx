d9edc0dfee130c51cbfd3d63d0de33d68b3d6b23
Vivian Zhang <vvynz12@gmail.com>
2024-06-13 00:39:22 -0400

    Update the Header and Navbar styling

--- client/src/container/Header/Header.jsx
+++ client/src/container/Header/Header.jsx
@@ -1,24 +1,14 @@
 import React from "react";
-import SVGInject from "@iconfu/svg-inject";
 
 import { images } from "../../constants";
 import HeaderUpdates from "./HeaderUpdates";
 
-import { Swiper, SwiperSlide } from "swiper/react";
-// Import Swiper React components
-import { Autoplay, Pagination, Navigation } from "swiper";
-// Import required Swiper modules
-
-import "swiper/scss";
-import "swiper/scss/pagination";
-import "swiper/scss/navigation";
-
-// import "./Header.scss";
+import "./Header.scss";
 
 export default function Header() {
   return (
     <section className="app__header-container">
-      {/* <img className="app__header_main" src={images.RPWPMain} /> */}
+      <img className="app__header-hero" src={images.RPWPMain} />
       <HeaderUpdates />
     </section>
   );
