fa24ed8981eb7a33cbd33f76f2d5abe36138be87
Vivian Zhang <vvynz12@gmail.com>
2024-06-28 00:01:29 -0400

    Fetch data from database and save to state

--- client/src/container/Discography/DIscographyMenu.jsx
+++ client/src/container/Discography/DIscographyMenu.jsx
@@ -1,8 +1,16 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { client, Client, urlFor } from "../../../client";
 
 import { motion } from "framer-motion";
 
 export default function DiscographyMenu() {
+  const [albums, setAlbums] = useState([]);
+
+  useEffect(() => {
+    const query = "*[_type == 'discography']";
+
+    client.fetch(query).then((data) => setAlbums(data));
+  }, []);
   return (
     <motion.section>
       <h1>DISCOGRAPHY</h1>
