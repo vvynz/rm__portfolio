e7583fdf83d55bfbe2a41d94b7ad624c24a26f1a
Vivian Zhang <vvynz12@gmail.com>
2024-06-27 23:54:29 -0400

    create a DiscographyMenu component

---
+++ client/src/container/Discography/DIscographyMenu.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+import { motion } from "framer-motion";
+
+export default function DiscographyMenu() {
+  return (
+    <motion.section>
+      <h1>DISCOGRAPHY</h1>
+      <div className="app__discography-menu-wrapper"></div>
+    </motion.section>
+  );
+}
