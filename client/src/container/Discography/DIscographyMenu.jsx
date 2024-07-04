3c3e291942352b8ef71f73bbc167420835d83ed0
Vivian Zhang <vvynz12@gmail.com>
2024-07-04 00:12:49 -0400

    DiscographyMenu component styling update

--- client/src/container/Discography/DIscographyMenu.jsx
+++ client/src/container/Discography/DIscographyMenu.jsx
@@ -3,6 +3,8 @@ import { client, urlFor } from "../../../client";
 
 import { motion } from "framer-motion";
 
+import "./DiscographyMenu.scss";
+
 export default function DiscographyMenu() {
   const [albums, setAlbums] = useState([]);
 
@@ -12,12 +14,16 @@ export default function DiscographyMenu() {
     client.fetch(query).then((data) => setAlbums(data));
   }, []);
   return (
-    <motion.section>
+    <motion.section className="app__discography-menu">
       <h1>DISCOGRAPHY</h1>
       <div className="app__discography-menu-wrapper">
         {albums.map((item, index) => (
-          <div key={index}>
-            <img src={urlFor(item.albumImg)} alt={item.albumImg.caption} />
+          <div className="app__discography-menu-album">
+            <img
+              className="app__discography-menu-album-img"
+              src={urlFor(item.albumImg)}
+              alt={item.albumImg.caption}
+            />
           </div>
         ))}
       </div>
